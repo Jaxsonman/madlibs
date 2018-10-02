53c859ae0396f8f513ff669564d1170241adf3e2
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-02 16:28:15 -0600

    built class comp

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = (custom) =>{
+    return(
+        <div className="input">
+            <input />
+            <label>{custom}</label>
+        </div>
+    )
+}
+
+export default Input;
+
